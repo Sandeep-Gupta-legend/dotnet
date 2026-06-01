189fee802772c8453ab4fe28f9a445640abfadea
Sandeep-Gupta-legend <sandeep1legend@gmail.com>
2026-06-01 10:52:33 +0530

    take input from user in list and dictionary

---
+++ fourth/Program.cs
@@ -0,0 +1,28 @@
+﻿List<string> list = new List<string>();
+for(int i=0; i<3; i++)
+{
+    Console.WriteLine("enter the name:");
+    
+    list.Add(Console.ReadLine());
+
+
+}
+foreach(string name in list)
+{
+    Console.WriteLine(name);
+}
+
+
+Dictionary<int,string> dict = new Dictionary<int,string>();
+for(int i=0; i<3; i++)
+{
+    Console.WriteLine("enter the id:");
+    int id = int.Parse(Console.ReadLine());
+    Console.WriteLine("enter the name:");
+    string name = Console.ReadLine();
+    dict.Add(id,name);
+}
+foreach(KeyValuePair<int,string> item in dict)
+{
+    Console.WriteLine($"ID: {item.Key}, Name: {item.Value}");
+}
