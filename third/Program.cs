72451eb8367bb5287e5b447ce9766c46ad45e0d4
Sandeep-Gupta-legend <sandeep1legend@gmail.com>
2026-06-01 10:42:59 +0530

    added list and dictionary

---
+++ third/Program.cs
@@ -0,0 +1,19 @@
+﻿List<string> Employees = new List<string>();
+Employees.Add("Sandeep");
+Employees.Add("Rohit");
+Employees.Add("Rahul");
+Console.WriteLine("Employees in the company:");
+foreach(string employee in Employees)
+{
+    Console.WriteLine(employee);
+}
+
+Dictionary<int,string> Employe = new Dictionary<int,string>();
+Employe.Add(1,"power");
+Employe.Add(2,"energy");
+Employe.Add(3,"strength");
+Console.WriteLine("Employee details:");
+foreach(KeyValuePair<int,string> emp in Employe)
+{
+    Console.WriteLine($"ID: {emp.Key}, Name: {emp.Value}");
+}
