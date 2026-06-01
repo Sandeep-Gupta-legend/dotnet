baf4c513e1d2f0990ae8fa70fb91877ab8075478
Sandeep-Gupta-legend <sandeep1legend@gmail.com>
2026-06-01 10:13:16 +0530

    dot net basic

---
+++ FirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("sandeep the legend");
+string name = "sandeep";
+int age= 20;
+
+Console.WriteLine($"My name is {name}");
+Console.WriteLine($"my age is {age}");
+
+Console.WriteLine("enter your name");
+String name2= Console.ReadLine();
+Console.WriteLine("Your name is " + name2);
+
+Console.WriteLine("enter your age");
+int age2= int.Parse(Console.ReadLine());
+Console.WriteLine("Your age is " + age2);
