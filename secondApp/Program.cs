c6b95ac1d12534fb64b45a4e290980cedcc29a59
Sandeep-Gupta-legend <sandeep1legend@gmail.com>
2026-06-01 10:23:36 +0530

    second program

---
+++ secondApp/Program.cs
@@ -0,0 +1,15 @@
+﻿String[] names = {"sandeep","rishi","shivam"};
+foreach(String name in names)
+{
+    Console.WriteLine(name);
+
+}
+
+Console.WriteLine("enter the number:");
+int number= int.Parse(Console.ReadLine());
+Console.WriteLine($"the multiplication table of {number} is:");
+for(int i=1;i<=10;i++)
+{
+    int b=i*number;
+    Console.WriteLine(b);
+}
